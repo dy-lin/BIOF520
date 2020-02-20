c98ef4d76e1a1cce53f6c8df527b284e8723fbe4
Diana Lin <dlin@bcgsc.ca>
2020-02-19 20:40:03 -0800

    Sort by country

--- assignments/hw02/case04_hw02_origins.sh
+++ assignments/hw02/case04_hw02_origins.sh
@@ -5,9 +5,9 @@ while read acc
 do
 	if [[ "$acc" == "AF372422.1" ]]
 	then
-		echo "$acc,unknown" >> case04_hw02_origins.csv
+		echo "$acc,unknown"
 		continue
 	fi
 	read country
-	echo "$acc,$country" >> case04_hw02_origins.csv
-done < <( ggrep -E 'VERSION|country' case04_hw02_genbank.gb | gsed 's/VERSION\s\+//' | gsed 's/\s\+\/country="//' | tr -d '"' | gawk -F ":" '{print $1}' | gsed "s/Cote d'Ivoire/Ivory Coast/" | gsed 's/Viet Nam/Vietnam/')
+	echo "$acc,$country"  
+done < <( ggrep -E 'VERSION|country' case04_hw02_genbank.gb | gsed 's/VERSION\s\+//' | gsed 's/\s\+\/country="//' | tr -d '"' | gawk -F ":" '{print $1}' | gsed "s/Cote d'Ivoire/Ivory Coast/" | gsed 's/Viet Nam/Vietnam/') | sort -t"," -k2,2 >> case04_hw02_origins.csv
