209292fada4fc15ea4a525a596a1f7e37346abce
Diana Lin <dlin@bcgsc.ca>
2020-02-19 23:46:36 -0800

    Add title to plot and R script

--- assignments/hw02/case04_hw02_tree.R
+++ assignments/hw02/case04_hw02_tree.R
@@ -13,10 +13,12 @@ metadata <- read_csv("case04_hw02_origins.csv", col_names = TRUE)
 ggtree(tree) %<+% metadata + 
   geom_tiplab(aes(color=Country), size = 3.5) + 
   theme(legend.position = c(0.8,0.2),
-        text = element_text(size =18)) + 
+        text = element_text(size =18),
+        plot.title = element_text(hjust = 0.5)) + 
   geom_treescale() +
   guides(color=guide_legend(ncol=3)) +
-  geom_cladelabel(node=49, label="top BLAST hit", colour = "red", fontsize = 4, offset=0.025, geom = "text")
+  geom_cladelabel(node=49, label="top BLAST hit", colour = "red", fontsize = 4, offset=0.025, geom = "text") +
+  ggtitle("Zika Virus: Envelope Genes from Various Countries")
  #  geom_text_repel(aes(label=node)) 
   # geom_cladelabel(node = 125, label = "Russia", offset = 0.08) +
   # geom_cladelabel(node = 140, label = "Senegal", offset = 0.08) +
