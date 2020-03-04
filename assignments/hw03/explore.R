c36ea96b2e7ba58a65ac4eb07f07f75026806226
Diana Lin <dlin@bcgsc.ca>
2020-03-03 19:08:36 -0800

    Add network code (commented)

--- assignments/hw03/explore.R
+++ assignments/hw03/explore.R
@@ -1,17 +1,18 @@
 library(tidyverse)
 library(ggtree)
+library(stringr)
 
 data <-
   read_csv(
     "BIOF520/assignments/hw03/data/zombieOutbreak.csv",
     col_types = cols(
-      ID = col_factor(),
+      ID = col_character(),
       status = col_factor(),
       dateOfOnset = col_date(format = "%d-%b-%y"),
       dateOfExposureStart = col_date(format = "%d-%b-%y"),
       dateOfExposureEnd = col_date(format = "%d-%b-%y"),
       incubation = col_factor(),
-      source = col_factor(),
+      source = col_character(),
       hospWard = col_factor(),
       exposureHousehold = col_factor(),
       exposureSharedMeal = col_factor(),
@@ -44,3 +45,17 @@ table(data$Occupation)
 table(data$Age)
 
 View(data)
+
+# processed_data <- data %>%
+#   mutate(source = str_remove(source, "Patient "))
+# 
+# nodes <- processed_data %>%
+#   select(ID) 
+#   
+# edges <- processed_data %>%
+#   select(ID, source) %>%
+#   rename(from = source, to = ID) 
+# 
+# map_network <- network(edges, vertex.attr = nodes, matrix.type = "edgelist", ignore.eval = FALSE)
+# 
+# plot(map_network, vertex.cex = 3)
